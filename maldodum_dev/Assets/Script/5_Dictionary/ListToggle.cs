209aeb98c2f5452e60fd35abab6b401cac3782f6
Lee-Yeonghyeon <yeonghyeon0403@naver.com>
2022-10-15 16:46:16 +0900

    [FEAT] 발음사전 책리스트뷰 구현

---
+++ maldodum_dev/Assets/Script/5_Dictionary/ListToggle.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListToggle : MonoBehaviour
+{
+    public Image list1;
+    public Image list2;
+    public Sprite select_list1;
+    public Sprite select_list2;
+    public Sprite unSelect_list1;
+    public Sprite unSelect_list2;
+    public bool isSelect1 = true;
+
+    GameObject cardChicken;
+    GameObject cardCook;
+
+    public void Changelist()
+    {
+        if (isSelect1)
+        {
+            isSelect1 = false;
+            PlayerPrefs.SetString("dictionary", "cookcook");
+            list1.sprite = unSelect_list1;
+            list2.sprite = select_list2;
+            cardChicken.SetActive(false);
+            cardCook.SetActive(true);
+        }
+        else
+        {
+            isSelect1 = true;
+            PlayerPrefs.SetString("dictionary", "chicken");
+            list1.sprite = select_list1;
+            list2.sprite = unSelect_list2;
+            cardChicken.SetActive(true);
+            cardCook.SetActive(false);
+        }
+
+    }
+
+    void Start()
+    {
+        this.cardChicken = GameObject.Find("CardList_NiceChicken");
+        this.cardCook = GameObject.Find("CardList_CookCookABC");
+
+        cardChicken.SetActive(true);
+        cardCook.SetActive(false);
+
+        //todo. 이전 화면에서 넘어올 경우, chicken이 기본값인 것 수정 필요
+        PlayerPrefs.SetString("dictionary", "chicken");
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ maldodum_dev/Assets/Script/5_Dictionary/ViewCard.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ViewCard : MonoBehaviour
+{
+
+    Vector2 startPos;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startPos = Input.mousePosition;
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            //우->좌 방향으로 스크롤하면 선택한 책에 대한 화면으로 이동
+            if (swipeLength < -350)
+            {
+                swipeLength = 0;
+
+                string dictionaryBook = PlayerPrefs.GetString("dictionary");
+                if (dictionaryBook.Equals("chicken"))
+                {
+                    SceneManager.LoadScene("9.2_NiceChicken");
+                } else if (dictionaryBook.Equals("cookcook")){
+                    SceneManager.LoadScene("9.3_CookCookABC");
+                }
+            }
+        }
+
+    }
+}
