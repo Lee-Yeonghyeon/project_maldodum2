ebe72bbaf16d4c0dfac63e50773e20d81c1b884e
Lee-Yeonghyeon <yeonghyeon0403@naver.com>
2022-10-24 00:57:33 +0900

    [FEAT] 9.캐릭터설정 디자인 배치

--- maldodum_dev/Assets/Script/SceneChange/Nav_SceneChange.cs
+++ maldodum_dev/Assets/Script/SceneChange/Nav_SceneChange.cs
@@ -6,43 +6,32 @@ using UnityEngine.UI;
 
 public class Nav_SceneChange : MonoBehaviour
 {
-    GameObject select_obj;
-
-    void Start()
-    {
-        this.select_obj = GameObject.Find("0_2 메뉴선택");
-    }
-
-    void Update()
-    {
-        
-    }
 
     public void MoveBookCase()
     {
         Debug.Log("move to bookcase");
-        select_obj.transform.position = new Vector3(-5.95f, 4.1f, 0f);
+        //select_obj.transform.position = new Vector3(-5.95f, 4.1f, 0f);
         SceneManager.LoadScene("2_BookCase");
     }
 
     public void MoveCharacter()
     {
         Debug.Log("move to character");
-        select_obj.transform.position = new Vector3(-5.95f, 2.85f, 0f);
-        //SceneManager.LoadScene("");
+        //select_obj.transform.position = new Vector3(-5.95f, 2.85f, 0f);
+        SceneManager.LoadScene("9_Character");
     }
 
     public void MoveDictionary()
     {
         Debug.Log("move to dictionary");
-        select_obj.transform.position = new Vector3(-5.95f, 1.6f, 0f);
+        //select_obj.transform.position = new Vector3(-5.95f, 1.6f, 0f);
         SceneManager.LoadScene("10.1_BookList");
     }
 
     public void MoveInformation()
     {
         Debug.Log("move to infomation");
-        select_obj.transform.position = new Vector3(-5.95f, 0.35f, 0f);
+        //select_obj.transform.position = new Vector3(-5.95f, 0.35f, 0f);
         //SceneManager.LoadScene("");
     }
 
