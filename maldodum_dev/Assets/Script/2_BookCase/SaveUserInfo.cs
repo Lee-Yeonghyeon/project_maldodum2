f568790e40f4377c0a669b22ae479de1d85feff7
Lee-Yeonghyeon <yeonghyeon0403@naver.com>
2022-11-29 10:01:51 +0900

    [FEAT] 전시용 기능 구현

--- maldodum_dev/Assets/Script/2_BookCase/SaveUserInfo.cs
+++ maldodum_dev/Assets/Script/2_BookCase/SaveUserInfo.cs
@@ -17,8 +17,23 @@ public class SaveUserInfo : MonoBehaviour
 
     public void stageReset()
     {
-        PlayerPrefs.SetString("Stage1", "incomplete");      //단계 완료시 complete, 미완료시 incomplete
-        PlayerPrefs.SetString("Stage2", "incomplete");
-        PlayerPrefs.SetString("Stage3", "incomplete");
+        PlayerPrefs.SetString("Stage1_seed", "incomplete");      //단계 완료시 complete, 미완료시 incomplete
+        PlayerPrefs.SetString("Stage2_seed", "incomplete");
+        PlayerPrefs.SetString("Stage3_seed", "incomplete");
+
+        PlayerPrefs.SetString("Stage1_spout", "incomplete");
+        PlayerPrefs.SetString("Stage2_spout", "incomplete");
+        PlayerPrefs.SetString("Stage3_spout", "incomplete");
+    }
+
+    public void stageComplete()
+    {
+        PlayerPrefs.SetString("Stage1_seed", "complete");
+        PlayerPrefs.SetString("Stage2_seed", "complete");
+        PlayerPrefs.SetString("Stage3_seed", "complete");
+
+        PlayerPrefs.SetString("Stage1_spout", "complete");
+        PlayerPrefs.SetString("Stage2_spout", "complete");
+        PlayerPrefs.SetString("Stage3_spout", "complete");
     }
 }
